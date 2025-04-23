36b81405f855a3cecb5e49cbfd0fe347f94f50ee
Ron Kad <ronka10@gmail.com>
2025-04-23 12:24:34 +0300

    closes #5 - add SecurityAdapter, fixes to coordinate in: UserService, StoreService, Security, PasswordEncoder and UserDTO

---
+++ src/main/java/com/SEGroup/Infrastructure/SecurityAdapter.java
@@ -0,0 +1,52 @@
+package com.SEGroup.Infrastructure;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SecurityAdapter implements IAuthenticationService{
+    @Autowired
+    Security sec;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public void checkSessionKey(String sessionKey) throws AuthenticationException{
+        if(!sec.validateToken(sessionKey)){
+            throw new AuthenticationException("Token is Invalid!");
+        }
+    }
+
+    @Override
+    public void invalidateSession(String sessionKey) throws AuthenticationException{
+        sec.makeTokenExpire(sessionKey);
+    }
+
+    @Override
+    public String getUserBySession(String sessionKey) throws AuthenticationException {
+        String userEmail = sec.extractUsername(sessionKey);
+        if(userEmail == null){
+            throw new AuthenticationException("Token is invalid!");
+        }
+        return userEmail;
+    }
+
+    @Override
+    public String authenticate(String email) {
+        return sec.generateToken(email);
+    }
+
+    @Override
+    public String encryptPassword(String password) {
+        return passwordEncoder.encrypt(password);
+    }
+
+    @Override
+    public void matchPassword(String encryptedPassword, String realPassword) throws AuthenticationException {
+        if(!passwordEncoder.checkPassword(realPassword, encryptedPassword)){
+            throw new AuthenticationException("Wrong password!");
+        }
+    }
+
+}
