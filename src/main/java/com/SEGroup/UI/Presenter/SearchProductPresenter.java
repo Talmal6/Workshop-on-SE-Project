52a5c120fd7ad9ae6cbc445507f6c8f16249594b
Omry Muadi <omrymuadi2@gmail.com>
2025-05-09 22:39:07 +0300

    changed searchProduct

--- src/main/java/com/SEGroup/UI/Presenter/SearchProductPresenter.java
+++ src/main/java/com/SEGroup/UI/Presenter/SearchProductPresenter.java
@@ -2,6 +2,7 @@ package com.SEGroup.UI.Presenter;
 
 import com.SEGroup.DTO.ShoppingProductDTO;
 import com.SEGroup.DTO.StoreDTO;
+import com.SEGroup.Domain.ProductCatalog.CatalogProduct;
 import com.SEGroup.Service.Result;
 import com.SEGroup.Service.StoreService;
 import com.SEGroup.UI.ServiceLocator;
@@ -13,6 +14,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.textfield.TextField;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -54,37 +56,42 @@ public class SearchProductPresenter {
 //                        .collect(Collectors.toList());
 //
 //        catalogView.showProducts(filtered);
-        for(StoreDTO storeDTO: ServiceLocator.getStoreService().viewAllStores().getData()) {
-            // for real products
-            Result<List<ShoppingProductDTO>> result =
-                    storeService.searchProducts(
-                            query,
-                            List.of(),     // no extra filters for now
-                            storeDTO.getName(),          // all stores
-                            List.of()      // no category filters
-                    );
-
-            if (result.isSuccess()) {
-                List<CatalogView.Product> products =
-                        result.getData()
-                                .stream()
-                                .map(dto -> new CatalogView.Product(
-                                        dto.getProductId(),
-                                        dto.getName(),
-                                        dto.getPrice(),
-                                        null,
-                                        dto.getCategory()
-                                ))
-                                .collect(Collectors.toList());
-
-                catalogView.showProducts(products);
-            } else {
-                Notification.show(
-                        "Search failed: " + result.getErrorMessage(),
-                        2500,
-                        Position.MIDDLE
-                );
-            }
+        List<CatalogView.Product> products = new LinkedList<>();
+        for(CatalogProduct cp : storeService.viewPublicProductCatalog().getData()){
+            products.add(new CatalogView.Product(cp.getCatalogID(),cp.getName(), 10.0,"",cp.getBrand()));
         }
+        catalogView.showProducts(products);
+//        for(StoreDTO storeDTO: ServiceLocator.getStoreService().viewAllStores().getData()) {
+//            // for real products
+//            Result<List<ShoppingProductDTO>> result =
+//                    storeService.searchProducts(
+//                            query,
+//                            List.of(),     // no extra filters for now
+//                            storeDTO.getName(),          // all stores
+//                            List.of()      // no category filters
+//                    );
+//
+//            if (result.isSuccess()) {
+//                List<CatalogView.Product> products =
+//                        result.getData()
+//                                .stream()
+//                                .map(dto -> new CatalogView.Product(
+//                                        dto.getProductId(),
+//                                        dto.getName(),
+//                                        dto.getPrice(),
+//                                        null,
+//                                        dto.getCategory()
+//                                ))
+//                                .collect(Collectors.toList());
+//
+//                catalogView.showProducts(products);
+//            } else {
+//                Notification.show(
+//                        "Search failed: " + result.getErrorMessage(),
+//                        2500,
+//                        Position.MIDDLE
+//                );
+//            }
+//        }
     }
 }
