d32fa8093f72f5a2e6829b9a4443a0e2135ce003
YehudaGoldshtein <yehudago@post.bgu.ac.il>
2025-05-25 19:28:09 +0300

    modified shipping interface to fit to external service, and added addressdto

--- src/main/java/com/SEGroup/Service/MockShippingService.java
+++ src/main/java/com/SEGroup/Service/MockShippingService.java
@@ -1,5 +1,6 @@
 package com.SEGroup.Service;
 
+import com.SEGroup.DTO.AddressDTO;
 import com.SEGroup.DTO.BasketDTO;
 import com.SEGroup.Domain.IShippingService;
 import org.springframework.stereotype.Service;
@@ -7,20 +8,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MockShippingService implements IShippingService {
+//    @Override
+//    public String ship(BasketDTO basket, String userEmail) {
+//        // Mock implementation
+//       return "Shipping products from " + basket.storeId() + " to " + userEmail;
+//    }
+//
+//    @Override
+//    public String cancelShipping(BasketDTO basket, String userEmail) {
+//        // Mock implementation
+//        return "Cancelling shipping from " + basket.storeId() + " to " + userEmail;
+//    }
+//
+//    @Override
+//    public String ship(String storeName, String productId, String userId) throws Exception {
+//        return null;
+//    }
     @Override
-    public String ship(BasketDTO basket, String userEmail) {
-        // Mock implementation
-       return "Shipping products from " + basket.storeId() + " to " + userEmail;
+    public Integer ship(AddressDTO address_detail){
+        return 0;
     }
 
     @Override
-    public String cancelShipping(BasketDTO basket, String userEmail) {
-        // Mock implementation
-        return "Cancelling shipping from " + basket.storeId() + " to " + userEmail;
-    }
-
-    @Override
-    public String ship(String storeName, String productId, String userId) throws Exception {
-        return null;
+    public Boolean cancelShipping(int shippingId){
+        return false;
     }
 }
